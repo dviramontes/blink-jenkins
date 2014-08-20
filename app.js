794910dbbdc52ecf5bcf549419d2d459f7981425
dviramontes <dviramontes@gmail.com>
2014-08-20 00:57:56 -0600

    adds shebang

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// install node-hid globally
-// npm -g install node-hid
+#! /home/pi/node-v0.10.2-linux-arm-pi/bin/node
 
 var PushBullet = require('pushbullet');
 var Blink1 = require('node-blink1');
@@ -7,44 +6,49 @@ var chalk = require('chalk');
 var pusher = new PushBullet('v1l0tai5v2UPm0k8RUj8lvkhppJBhBWVbXujxSgudnBWS');
 var pushStream = pusher.stream()
 
-pushStream.connect();
-
-var msg,
-    devices = Blink1.devices().length,
+var devices = Blink1.devices().length,
     fadeMillis = 1000,
     blink1, blinker;
 
-msg = devices > 0 ? "blink usb connected" : "device connected ??"
+pushStream.connect();
+console.log(chalk.blue("PushBullet stream connected"));
 
-if (devices > 0) {
+var devicePresent = devices > 0
+
+var msg = !!devicePresent ? "blink(1) usb connected" : "blink(1) device connected ??"
+
+if (devicePresent) {
     blink1 = new Blink1();
+    console.log(chalk.red(msg));
+} else {
+    console.log(chalk.red(msg), "exiting..");
+    process.exit(0) // normal exit
 }
 
-console.log(chalk.red(msg));
-console.log(chalk.blue("PushBullet stream connected"));
 
 pushStream.on('push', function(push) {
     console.log(push)
     // if up
-    if (push && push.body && push.body.indexOf('jenkinsup') !== -1) {
-        console.log('jenkins-is-up');
-        blinker('red');
-    } else {
-        // if down
-        console.log('jenkins-is-down');
-        blinker('blue');
+    if (push && push.body) {
+        if (push.body.indexOf('jenkinsup') !== -1) {
+            console.log('jenkins-is-up');
+            blinker('red');
+        } else if (push.body.indexOf('jenkinsup') !== -1) {
+            // if down
+            console.log('jenkins-is-down');
+            blinker('blue');
+        }
     }
-
 })
 
 function blinker(color) {
-    blink1.fadeToRGB(250,0,0,0)
-    blink1.fadeToRGB(500,255,125,255)
-    blink1.fadeToRGB(250,0,0,0)
-    blink1.fadeToRGB(500,0,125,255)
-    blink1.fadeToRGB(250,0,0,0)
-    blink1.fadeToRGB(500,50,255,0)
-    blink1.fadeToRGB(250,0,0,0)
+    blink1.fadeToRGB(250, 0, 0, 0)
+    blink1.fadeToRGB(500, 255, 125, 255)
+    blink1.fadeToRGB(250, 0, 0, 0)
+    blink1.fadeToRGB(500, 0, 125, 255)
+    blink1.fadeToRGB(250, 0, 0, 0)
+    blink1.fadeToRGB(500, 50, 255, 0)
+    blink1.fadeToRGB(250, 0, 0, 0)
 }
 
 blinker()
